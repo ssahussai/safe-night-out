b74ee51b6b9f36ed002bff66bc440e9659787004
Alex Perkowski <perkowskialex@gmail.com>
2019-12-20 14:05:13 -0600

    register models

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Drink, DrinkSession, Photo, Profile
 
 # Register your models here.
+admin.site.register(DrinkSession)
+admin.site.register(Drink)
+admin.site.register(Photo)
+admin.site.register(Profile)
